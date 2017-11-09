9488b52ebdc8a862038fb61b8c00403808bd94e3
bengarvey <bgarvey@rjmetrics.com>
2017-11-09 11:30:59 -0500

    added Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => (
+  <div>
+    <ul className="navList">
+      <li className="navListItem"><a href="">Home</a></li>
+      <li className="navListItem"><a href="about">About</a></li>
+      <li className="navListItem"><a href="https://twitter.com/bengarvey">Twitter</a></li>
+    </ul>
+  </div>
+)
+
+export default Home
