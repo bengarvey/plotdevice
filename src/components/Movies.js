5b5faa7cf72f3d6477c2c73fff086de2990d316c
bengarvey <bgarvey@rjmetrics.com>
2018-01-27 14:02:08 -0500

    added notes

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,10 +6,18 @@ import Nav from './Nav';
 const colors = {
   female: '#faa1c7',
   male: '#009ddc',
-  unknown: '#666666'
+  unknown: '#666666',
+  success: '#15b097',
+  failure: '#da4167',
+  neutral: '#999999',
+  primary: '#000000'
 }
 
 var movies = require('../data/topmovies.json');
+
+var decades = {};
+var directors = {};
+var genders = {};
 
 movies.forEach((m) => {
   Object.keys(m.rank).forEach((key) => {
@@ -28,22 +36,12 @@ function sortByRank(x,y,key) {
   return (a === null) - (b === null) || +(a > b) || -(a < b);
 }
 
-console.log(sortedMovies09);
-console.log(sortedMovies18);
-
 movies = movies.splice(0,99);
-const heatScale = scaleLinear()
-  .domain([0,5,10])
-  .range(["#fbf7da", "red", "darkred"]).clamp(true);
 
-const daysAxis = {
+const leftAxis = {
     orient: 'left', ticks: 7,
     tickFormat: d => d ?
     <text style={{ textAnchor: "end" }} fontSize="12px" y={5} x={2}>{d}</text> : '' };
-
-var decades = {};
-var directors = {};
-var genders = {};
 
 movies.forEach( function(d, i) {
   var decade = Math.floor( (new Date(d.release_date).getYear()/10) )*10 + 1900;
@@ -111,7 +109,17 @@ var display = {
 
 var slope = [
 ];
+
 var movieNames = [];
+var maxGain = {
+  diff: 0,
+  index: 0
+};
+
+var maxLoss = {
+  diff: 100,
+  index: 0
+};
 
 movies.forEach( function(m, i) {
   var item = {
@@ -128,38 +136,77 @@ movies.forEach( function(m, i) {
     {value: parseInt(m.rank['2018']), date: 2018, name: m.title}
   ];
 
+  if (m.rank["2009"] != null && m.rank["2018"] != null) {
+    var diff = parseInt(m.rank['2009'] - m.rank['2018']);
+
+    if (maxGain.diff < diff) {
+      maxGain = {
+        diff: diff,
+        index: i
+      };
+    };
+
+    if (maxLoss.diff > diff) {
+      maxLoss = {
+        diff: diff,
+        index: i
+      };
+    };
+  }
+
   var slopeItem = [
-    {data: line, name: m.title}
+    {data: line}
   ];
+
+  slopeItem.color = colors.neutral;
+  slopeItem.strokeWidth = '1px';
+  slopeItem.name = m.title;
 
   slope.push(slopeItem);
   movieNames.push(m.title);
 });
 
-/*
-var display = {
-  data: [
-    {'rank2018': "0.5", vote_average: 0.3, name: "Ben"},
-    {'rank2018': "0.2", vote_average: 0.7, name: "Sasha"},
-    {'rank2018': "0.9", vote_average: 0.9, name: "Owen"}
-  ]
-};
-*/
-
-display.data.forEach( function(m) {
-  //console.log(m.rank['2018'], m.vote_average);
-});
+slope[maxGain.index].color = colors.success;
+slope[maxGain.index].strokeWidth = '3px';
+slope[maxLoss.index].color = colors.failure;
+slope[maxLoss.index].strokeWidth = '3px';
 
 function getName(index, lookup) {
   var title = index >= 100 ? index : lookup[index].title;
-  return title.length > 17 ? title.substr(0,15) + '...' : title;
+  return (title.length > 17 ? title.substr(0,15) + '...' : title);
 }
 
-console.log(slope);
+function getColor(index, lookup, year) {
+  return year === '2018' ? getColorLeft(index, lookup, year) : getColorRight(index, lookup, year);
+}
+
+function getColorLeft(index, lookup, year) {
+  return lookup[index].rank[year] == null ? colors.failure : colors.neutral;
+}
+
+function getColorRight(index, lookup, year) {
+  return lookup[index].rank[year] == null ? colors.success : colors.neutral;
+}
+
+function getAxisClass(d, year) {
+  return year === 2009 ? getAxisClassLeft(d, year) : getAxisClassRight(d, year);
+}
+
+function getAxisClassLeft(d, year) {
+  return d.rank[year] === null ? 'failure' : '';
+}
+
+function getAxisClassRight(d, year) {
+  return d.rank[year] === null ? 'success' : '';
+}
+
+function showLine(d) {
+  return !isNaN(d.value);
+}
 
 const Movies = () => (
   <div className="chartContainer">
-    <h1>My Top 100 Movies</h1>
+    <h1>My Top 100 Favorite Movies</h1>
     <h3>When were they made?</h3>
     <ResponsiveORFrame
       size={[ 360, 200 ]}
@@ -168,10 +215,11 @@ const Movies = () => (
       projection={'vertical'}
       rAccessor={d => d.value}
       oAccessor={d => d.name}
-      hoverAnnotation={true}
-      style={d => ({ fill: '#000000', stroke: "#000000", strokeOpacity: 0.0, fillOpacity: 0.5, strokeWidth: 2 })}
+      pieceHoverAnnotation={true}
+      tooltipContent={ d => `${d.value} ${d.name}'s films` }
+      style={d => ({ fill: colors.primary, stroke: colors.primary, strokeOpacity: 0.0, fillOpacity: 0.5, strokeWidth: 2 })}
       type={"bar"}
-      axis={daysAxis}
+      axis={leftAxis}
       oLabel={(d) => (<text textAnchor="middle">{d}'s</text>)}
       margin={{ left: 30, top: 10, bottom: 30, right: 10 }}
       oPadding={5}
@@ -184,12 +232,13 @@ const Movies = () => (
       projection={'horizontal'}
       rAccessor={d => d.value}
       oAccessor={d => d.name}
-      hoverAnnotation={true}
-      style={d => ({ fill: '#000000', stroke: "#000000", strokeOpacity: 0.0, fillOpacity: 0.5, strokeWidth: 2 })}
+      pieceHoverAnnotation={true}
+      tooltipContent={ d => `${d.value} filmss by ${d.name}` }
+      style={d => ({ fill: colors.primary, stroke: colors.primary, strokeOpacity: 0.0, fillOpacity: 0.5, strokeWidth: 2 })}
       type={"bar"}
-      axis={daysAxis}
+      axis={leftAxis}
       oLabel={(d, i) => (<text x={0} y={3} className={i[0].className} textAnchor="end">{d}</text>)}
-      margin={{ left: 130, top: 10, bottom: 50, right: 10 }}
+      margin={{ left: 130, top: 0, bottom: 50, right: 10 }}
       oPadding={2}
     />
     <h3>How have my choices changed since 2009</h3>
@@ -197,6 +246,7 @@ const Movies = () => (
       size={[360,1200]}
       lines={slope}
       responsiveWidth={true}
+      defined={d => showLine(d)}
       yExtent={[100,1]}
       lineDataAccessor={d => d[0].data}
       yAccessor={"value"}
@@ -204,18 +254,21 @@ const Movies = () => (
       lineType={{type: 'line'}}
       hoverAnnotation={true}
       tooltipContent={ d => `${d.value} ${d.name}` }
-      lineStyle={(d) => ({ stroke: '#000', strokeWidth: '1px', opacity:'0.3' })}
+      lineStyle={(d) => ({ stroke: d.color, strokeWidth: d.strokeWidth, opacity:'0.3' })}
       margin={{"top":10,"bottom":40,"left":100,"right":100}}
       axes={[
-    { className: 'slopeTick', orient: 'left', tickFormat: d =>  getName(d-1, sortedMovies09).substr(0,40), ticks: 100},
-    { clssName: 'slopeTick', orient: 'right', tickFormat: d => getName(d-1, sortedMovies18).substr(0,40), ticks: 100},
-    { className: 'slopeTick', orient: 'bottom', tickFormat: d => d }
+        { className: 'slopeTick', orient: 'left', tickFormat: d => <text textAnchor="end" style={{fill: getColor(d-1, sortedMovies09, '2018')}}>{getName(d-1, sortedMovies09).substr(0,40)}</text>, ticks: 100},
+        { className: 'slopeTick', orient: 'right', tickFormat: d => <text style={{fill: getColor(d-1, sortedMovies18, '2009')}}>{getName(d-1, sortedMovies18).substr(0,40)}</text>, ticks: 100},
+        { className: 'slopeTick', orient: 'bottom', tickFormat: d => d }
       ]}
       hoverAnnotation={true}
     />
     <div className="notes nextReport">
       <h3>Notes and Sources</h3>
-      <p>Tech: <a href="https://emeeks.github.io/semiotic/#/">Semiotic</a>, javascript, <a href="themoviedb.org">TheMovieDB.org</a></p>
+      <p>Way back in <a href="http://bengarvey.com/2009/06/21/Ben-Garveys-100-Greatest-Movies-of-all-Time/">2009</a> I made a list of my top 100 favorite movies. I recently rewrote the list for <a href="http://bengarvey.com/2018/01/14/top-100-movies/">2018</a> and wanted to run some stats on it and build a <a href="https://www.edwardtufte.com/bboard/q-and-a-fetch-msg?msg_id=0003nk">Tufte stype slopegraph</a> to show the changes. Coloring the axis tick labels was the toughest part of the whole project and required me to file a <a href="https://github.com/emeeks/semiotic/issues/128">github issue</a> about it. Red movies fell off the list and green ones were added.</p>
+      <p>I grew up in the 80s and 90s, so it's surprising to me the 70s fared better than the 80s. I blame recency bias for how well the 00s are represented. At some point I should add an annotation for my favorite film from each decade.</p>
+      <p>All the meta data comes from a great API called <a href="https://themoviedb.org">TheMovieDB.org</a>. I'll include the scripts I used to scrape the data in a future pull request. At one point my script pulled information from a different Life is Beautiful and mixed up the LEGO movie with the Lego Ninjago Movie, which I haven't seen but I'm sure is not as good as the LEGO movie.</p>
+      <p>Tech: <a href="https://emeeks.github.io/semiotic/#/">Semiotic</a>, My top 100 movies from <a href="http://bengarvey.com/2009/06/21/Ben-Garveys-100-Greatest-Movies-of-all-Time/">2009</a> and <a href="http://bengarvey.com/2018/01/14/top-100-movies/">2018</a>, javascript, <a href="https://themoviedb.org">TheMovieDB.org</a></p>
     </div>
     <Nav/>
   </div>
