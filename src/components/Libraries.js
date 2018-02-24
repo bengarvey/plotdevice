2800fd1c3ab46e76561622548b981fd783aba765
bengarvey <bgarvey@rjmetrics.com>
2018-02-24 11:08:09 -0500

    fixed Libraries

--- src/components/Libraries.js
+++ src/components/Libraries.js
@@ -24,19 +24,7 @@ const colors = {
   gasAdjusted: '#d86641'
 }
 
-var modified = {
-  lib: []
-};
-
-libs.forEach( function(d) {
-  modified.lib.push(
-    {x: d.flex, y:d.speed, name: d.name, logo: d.logo, cy:-20});
- });
-
-var display =
-  {data: modified.lib, color: colors.lib, renderMode: "normal"};
-
-var sharedProps = {
+const sharedProps = {
   size: [100, 350],
   xAccessor: "x",
   yAccessor: "y",
@@ -44,34 +32,61 @@ var sharedProps = {
   margin:{ left: 10, bottom: 30, right: 10, top: 10 }
 };
 
-const Libraries = () => (
-  <div className="chartContainer">
-    <h1>Charting Technolgies</h1>
-    <h3>Tradeoff between speed and flexibility</h3>
-    <ResponsiveXYFrame
-      { ...sharedProps }
-      responsiveWidth={true}
-      yExtent={[0,1]}
-      xExtent={[0,1]}
-      points={display.data}
-      pointStyle={ d => ({fill: "#666", r: '5px'})}
-      tooltipContent={d => d.name}
-      customPointMark={ ({d}) => ( <Mark markType="image" x={-10} y={-10} transform="translate(-20,-20)" height="20px" width="20px" type="image/svg+xml" xlinkHref={d.logo}/> ) }
-      axes={[
-        { orient: 'bottom', padding: 0, ticks: 5, tickFormat: d => ''},
-        { orient: 'left', ticks: 5, tickFormat: d => ''}
-      ]}
-      margin={{ left: 10, bottom: 10, right: 10, top: 10 }}
-    />
-    <div className="notes nextReport">
-      <h3>Notes and Sources</h3>
-      <p>This is a fun scatter plot I used in my <a href="https://docs.google.com/presentation/d/1OEHDz99ieT50piYyQRYx9msShZs1aQFVnOyRVMOJxVg/edit?usp=sharing">BarCampPhily slides</a> to illustrate where Semiotic fits in with other chart libraries like Highcharts, Plotly, D3, etc. The data is 100% made up by me.</p>
-      <p>The point is that I can make a chart in Excel or Google Sheets quickly and it will be ugly and boring, or I can spent months hammering it out in D3 or vanilla Javascript and it will be beautiful. Semiotic cherry picks some of the cooler D3 chart types and makes them more accessible and easier to work with.</p>
-      <p>Making this chart was hard, because I had no idea how to get the custom logos in there. I ended up filing a <a href="https://github.com/emeeks/semiotic/issues/64">Github issue about it</a> amd they helped me figure it out.</p>
-      <p>Tech: <a href="https://emeeks.github.io/semiotic">Semiotic</a>, javascript, GIMP</p>
-    </div>
-    <Nav/>
-  </div>
-);
+class Libraries extends React.Component {
+  constructor(props) {
+    super(props);
+    this.display = {};
+    this.process();
+  }
+
+  process() {
+    var modified = {
+      lib: []
+    };
+
+    libs.forEach( function(d) {
+      modified.lib.push(
+        {x: d.flex, y:d.speed, name: d.name, logo: d.logo, cy:-20});
+     });
+
+    this.display = {
+      data: modified.lib,
+      color: colors.lib,
+      renderMode: "normal"
+    };
+  }
+
+  render() {
+    return (
+      <div className="chartContainer">
+        <h1>Charting Technolgies</h1>
+        <h3>Tradeoff between speed and flexibility</h3>
+        <ResponsiveXYFrame
+          { ...sharedProps }
+          responsiveWidth={true}
+          yExtent={[0,1]}
+          xExtent={[0,1]}
+          points={this.display.data}
+          pointStyle={ d => ({fill: "#666", r: '5px'})}
+          tooltipContent={d => d.name}
+          customPointMark={ ({d}) => ( <Mark markType="image" x={-10} y={-10} transform="translate(-20,-20)" height="20px" width="20px" type="image/svg+xml" xlinkHref={d.logo}/> ) }
+          axes={[
+            { orient: 'bottom', padding: 0, ticks: 5, tickFormat: d => ''},
+            { orient: 'left', ticks: 5, tickFormat: d => ''}
+          ]}
+          margin={{ left: 10, bottom: 10, right: 10, top: 10 }}
+        />
+        <div className="notes nextReport">
+          <h3>Notes and Sources</h3>
+          <p>This is a fun scatter plot I used in my <a href="https://docs.google.com/presentation/d/1OEHDz99ieT50piYyQRYx9msShZs1aQFVnOyRVMOJxVg/edit?usp=sharing">BarCampPhily slides</a> to illustrate where Semiotic fits in with other chart libraries like Highcharts, Plotly, D3, etc. The data is 100% made up by me.</p>
+          <p>The point is that I can make a chart in Excel or Google Sheets quickly and it will be ugly and boring, or I can spent months hammering it out in D3 or vanilla Javascript and it will be beautiful. Semiotic cherry picks some of the cooler D3 chart types and makes them more accessible and easier to work with.</p>
+          <p>Making this chart was hard, because I had no idea how to get the custom logos in there. I ended up filing a <a href="https://github.com/emeeks/semiotic/issues/64">Github issue about it</a> amd they helped me figure it out.</p>
+          <p>Tech: <a href="https://emeeks.github.io/semiotic">Semiotic</a>, javascript, GIMP</p>
+        </div>
+        <Nav/>
+      </div>
+    );
+  }
+}
 
 export default Libraries
