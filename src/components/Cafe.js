6312aa973bb7d19fd6a78045c5cef6122b97c693
bengarvey <bgarvey@rjmetrics.com>
2019-04-26 23:18:19 -0400

    kind of got it working

--- src/components/Cafe.js
+++ src/components/Cafe.js
@@ -3,6 +3,7 @@ import { ResponsiveXYFrame } from 'semiotic';
 import Nav from './Nav';
 import { curveBasis } from 'd3-shape';
 import { AnnotationLabel } from 'react-annotation';
+import Categrid from './Categrid';
 var transactions = require('../data/owenbudget.json');
 
 const formatter = new Intl.DateTimeFormat("en", { month: "short" } );
@@ -43,7 +44,6 @@ class Cafe extends React.Component {
 
     this.display[0].data.sort( (a, b) => a.date - b.date );
     this.display[0].data.map( (item, i) => {item.id = i; return item;});
-    console.log(this.display);
   }
 
   render() {
@@ -69,6 +69,7 @@ class Cafe extends React.Component {
           margin={{top: 10, left: 30, right: 45, bottom: 50}}
           annotations={annotations}
         />
+        <Categrid data={[5,10,1,3]} size={[500,500]}/>
         <div className="notes nextReport">
           <h3>Notes and Sources</h3>
           <p>This is my son's cafeteria transaction history so far for the 2018 school.</p>
