8a2fd55a32edb1841ac662396b10e249cbe23951
bengarvey <bgarvey@rjmetrics.com>
2020-08-09 10:43:27 -0400

    fixed some typos

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,7 +21,7 @@ const Home = () => (
       <li className="list-item"><a href="cafe">Owen's Cafeteria Expenses</a></li>
       <li className="list-item"><a href="meetup">Meetup Hex Colors</a></li>
       <li className="list-item"><a href="dungeonadventure">Dungeon Adventure Sales</a></li>
-      <li className="list-item"><a href="autodeaths">Auto Deaths</a></li>
+      <li className="list-item"><a href="autodeaths">US Autombile Fatalities 1899 - 2018</a></li>
     </ul>
     <div>
       <ul className="navList">
